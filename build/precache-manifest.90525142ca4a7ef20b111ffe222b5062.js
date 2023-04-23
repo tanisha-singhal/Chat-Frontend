c7c30fa19ceda30b31d988f693cf3ead9d48b8fc
tanisha-singhal <tanusinghal1000@gmail.com>
2023-04-23 20:23:20 +0530

    deploy

---
+++ build/precache-manifest.90525142ca4a7ef20b111ffe222b5062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2ac2e00b6be804d4e03",
+    "url": "/static/css/main.f3f4fb7d.chunk.css"
+  },
+  {
+    "revision": "d2ac2e00b6be804d4e03",
+    "url": "/static/js/main.d2ac2e00.chunk.js"
+  },
+  {
+    "revision": "2a09fdb3cdac35b68421",
+    "url": "/static/js/1.2a09fdb3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7570c3893db910d3bb5d65c7f597bc83",
+    "url": "/index.html"
+  }
+];
